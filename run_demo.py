859abe68ebf0879a2d7ae7fb6061a64889377b92
Gustavo Magalhães <gumagalcomp77@gmail.com>
2023-07-17 15:32:13 -0300

    Use our topology

--- run_demo.py
+++ run_demo.py
@@ -1,7 +1,8 @@
+from logging import info
 from mininet.net import Mininet
 from mininet.link import TCLink
-from mininet.node import RemoteController
-from mininet.topo import SingleSwitchTopo, Topo
+from mininet.node import Docker, RemoteController
+from mininet.topo import Topo
 from mininet.cli import CLI
 
 from src.routing.mininet_based.routing import StaticRouter 
@@ -17,30 +18,45 @@ with open("tools/sflow-rt/extras/sflow.py") as f:
 def main():
     ### Init network
 
-    #topo = SingleSwitchTopo(2)
-    #topo = NSFNet() # Our NSFNet topology implementation
-
     topo=Topo()
 
-    #Create Nodes
-    topo.addHost("h1")
-    topo.addHost("h2")
-    topo.addHost("h3")
-    topo.addHost("h4")
-    topo.addSwitch('s1')
-    topo.addSwitch('s2')
-    topo.addSwitch('s3')
+    info('*** Adding docker containers\n')
 
-    #Create links
-    topo.addLink('s1','s2')
-    topo.addLink('s1','s3')
-    topo.addLink('s2','s3')
-    topo.addLink('h1','s1')
-    topo.addLink('h2','s2')
-    topo.addLink('h3','s3')
-    topo.addLink('h4','s3')
-    
-    net = Mininet(topo=topo, link=TCLink, controller=RemoteController)
+    gNodeB1 = topo.addHost('h1', cls=Docker, ip='10.0.0.2', dimage="ubuntu:trusty")
+    gNodeB2 = topo.addHost('h2', cls=Docker, ip='10.0.0.3', dimage="ubuntu:trusty")
+
+    bbudc = topo.addHost('h3',cls=Docker, ip='10.0.0.4', dimage="ubuntu:trusty")
+    regionaldc = topo.addHost('h4', cls=Docker, ip='10.0.0.5', dimage="ubuntu:trusty")
+    coredc = topo.addHost('h5',cls=Docker, ip='10.0.0.6', dimage="ubuntu:trusty")
+
+    info('*** Adding switches\n')
+    s1 = topo.addSwitch('s1')
+    s2 = topo.addSwitch('s2')
+    s3 = topo.addSwitch('s3')
+    s4 = topo.addSwitch('s4')
+    s5 = topo.addSwitch('s5')
+    s6 = topo.addSwitch('s6')
+    info('*** Creating links\n')
+    topo.addLink(gNodeB1, s1)
+    topo.addLink(gNodeB2, s2)
+    topo.addLink(s1, bbudc, delay='0.5ms', bw=1000)
+    topo.addLink(s2, bbudc, delay='0.5ms', bw=1000)
+    topo.addLink(s1, s3, delay='3ms', bw=1000)
+    topo.addLink(s2, s4, delay='3ms', bw=1000)
+    topo.addLink(s3, regionaldc, delay='2ms', bw=1000)
+    topo.addLink(s4, regionaldc, delay='2ms', bw=1000)
+    topo.addLink(s3, s5, delay='11ms', bw=1000)
+    topo.addLink(s4, s6, delay='11ms', bw=1000)
+    topo.addLink(s5, coredc, delay='11ms', bw=1000)
+    topo.addLink(s6, coredc, delay='11ms', bw=1000)
+
+    topo.addLink(s1, s4, delay='3ms', bw=1000)
+    topo.addLink(s2, s3, delay='3ms', bw=1000)
+    topo.addLink(s3, s6, delay='11ms', bw=1000)
+    topo.addLink(s4, s5, delay='11ms', bw=1000)
+
+    net = Mininet(topo=topo, controller=None, link=TCLink, autoSetMacs=True, autoStaticArp=True)
+    net.addController('c0',controller=RemoteController, ip='127.0.0.1', port=6653)
     net.start()
 
     ### Routing
