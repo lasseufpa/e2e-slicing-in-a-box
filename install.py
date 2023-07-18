e401e894f28155369452691b62b51746807ba8a1
Gustavo Magalhães <gumagalcomp77@gmail.com>
2023-07-18 20:04:24 -0300

    Add jpeg support for matplotlib

--- install.py
+++ install.py
@@ -24,7 +24,7 @@ os.chdir(setup_path)
 print("Step 1. Install prerequisites")
 
 run_command('sudo  apt-get -y install ansible  git  aptitude  gcc  python3-dev  libffi-dev  libssl-dev  libxml2-dev  libxslt1-dev  zlib1g-dev  openjdk-8-jre  adduser  libfontconfig1  debian-keyring  debian-archive-keyring  apt-transport-https')
-run_command('sudo  apt-get -y install  tmux')
+run_command('sudo  apt-get -y install  tmux libjpeg-dev zlib1g-dev')
 run_command('python3  -m  pip  install  pyyaml')
 run_command('python3  -m  pip  install  pandas')
 run_command('python3  -m  pip  install  matplotlib')
