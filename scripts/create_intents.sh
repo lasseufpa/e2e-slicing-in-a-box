b74bf45d7c8a94f5e5f03f58662e8f439d64b8a9
Gustavo Magalhães <gumagalcomp77@gmail.com>
2023-08-02 14:41:58 -0300

    Move ONOS scripts and configuration to controller

--- scripts/create_intents.sh
+++ scripts/create_intents.sh
@@ -1,6 +1,11 @@
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+CONTROLLER_DIR=$(dirname $SCRIPT_DIR)/controller
+
 ONOS_SSH="sshpass -p karaf ssh -n -p 8101 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no karaf@localhost"
 ONOS_CMD_FILE="onossdn"
-ONOS_ENV="onosdevices"
+ONOS_ENV="onosdevices.sh"
+
+cd $CONTROLLER_DIR
 
 source $ONOS_ENV
 
